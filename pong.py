9e3d45d1b34ba5889ff5e44324f676475bdf8bfc
Yhorm <aguilar_andres@protonmail.com>
2019-01-12 10:09:42 -0800

    added collision between ball & P2 paddle

--- pong.py
+++ pong.py
@@ -120,3 +120,9 @@ while True:
   if ball.xcor() < -390:
     ball.goto(0, 0)
     ball.dx *= -1
+
+  # Paddle and ball collisions
+  if (ball.xcor() > 340 and ball.xcor() < 350) and \
+          (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+    ball.setx(340)
+    ball.dx *= -1
